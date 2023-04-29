9d540e150bda19ba1ad0acd137dcffdd54f5edda
ZeyuanLiu <74821988+insomnia-hum@users.noreply.github.com>
2023-04-29 11:11:16 +0800

    R code for plotting Figure 3b

--- air map.R
+++ air map.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(tidyr)
 library(tensorflow)
 library(keras)
-
+#loaded the CMAQ-CNN model
 model = load_model_hdf5("my_model.h5")
 
 #'so2','nox','voc','pm25','pm10','nh3'
@@ -42,13 +42,14 @@ inputdata[,,5,3] = df2power[i,9]
 box[[v]] = inputdata
 }
 
-
-city = read_sf('C:/pHD刘泽源/区划/市.shp')
+#loaded the shp file of Chinese city
+city = read_sf('city.shp')
 library(readxl)
 for (i in 1:8) {
 pred = predict(model,box[[i]])%>%as.numeric()
-ms = read_xlsx('C:/pHD刘泽源/刘泽源/manuscript2/2018版经纬度.xlsx')
-index = read_xlsx('C:/pHD刘泽源/刘泽源/manuscript2/coeff2.xlsx')
+#loaded the coordination of Chinese air quality monitor stations 
+ms = read_xlsx('/2018版经纬度.xlsx')
+index = read_xlsx('/coeff2.xlsx')
 df = data.frame(areaname=index$Areaname,pm25 = pred)
 ms = filter(ms,areaname %in% df$areaname)
 ms = left_join(ms,df,by='areaname')
@@ -59,15 +60,21 @@ ms = ms[,c(1,2,3,4,7,8,9,6)]
 
 names(ms) = c('市','Monitor Description','Latitude','Longitude','Metric','Seasonal Metric','Statistic','Values')
 ms2 = ms%>%group_by(市)%>%summarise(pm = mean(Values))
+#joint the PM2.5 concentrations with city shp file correspondingly
 city = left_join(city,ms2,by='市')
 }
+#rename the city shap file
 names(city) = c("省代码","省","市代码","市","类型","geometry",'Baseline','CCUS0','CCUS1','CCUS2','CCUS2-NET1','CCUS2-NET2',
                 'CCUS2-NET2-MFRT','CCUS0-MFRT')
-ninedashline = read_sf('C:/pHD刘泽源/区划/海岸线.shp')
+#loaded the shp file of the Nine-dash line
+ninedashline = read_sf('/海岸线.shp')
 
 city2 = city%>%gather(key = 'scenario',value = 'pm',7:14)
-grid = read_sf('C:/pHD刘泽源/刘泽源/manuscript2/grid/grid.shp')
-ggplot(grid)+geom_sf(data = city2,aes(fill = pm))+scale_fill_viridis_b(direction = -1)+geom_sf(data = ninedashline)+facet_wrap(~factor(scenario,levels = c('Baseline','CCUS0','CCUS1','CCUS2','CCUS2-NET1','CCUS2-NET2','CCUS2-NET2-MFRT','CCUS0-MFRT')),
+#loaded the shp file with set projection
+grid = read_sf('/grid.shp')
+ggplot(grid)+geom_sf(data = city2,aes(fill = pm))+scale_fill_viridis_b(direction = -1)+
+geom_sf(data = ninedashline)+
+facet_wrap(~factor(scenario,levels = c('Baseline','CCUS0','CCUS1','CCUS2','CCUS2-NET1','CCUS2-NET2','CCUS2-NET2-MFRT','CCUS0-MFRT')),
                                                                                                                               nrow = 2,ncol = 4)+
   labs(fill = expression(paste(PM[2.5],' concentration (μg/m3)')))+theme_bw()+theme(legend.position = 'top',
                                                                                    strip.text = element_text(size=12,face = 'bold'),
@@ -75,6 +82,6 @@ ggplot(grid)+geom_sf(data = city2,aes(fill = pm))+scale_fill_viridis_b(direction
   coord_sf(xlim = c(-2500000,2500000),ylim = c(-2500000,2500000),
            expand = c(0))
 
-ggsave('./airmap.png',height = 8,width = 10,dpi = 600)
+
 
 
